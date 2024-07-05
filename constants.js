97679c98de34cbe50530aacbbc715afa23025826
Chris X <120683055+chrisxadvocatus@users.noreply.github.com>
2024-07-04 21:31:58 -0400

    daily update

--- constants.js
+++ constants.js
@@ -5,15 +5,18 @@ export const haricotsVerts = "haricots verts";
 
 export const peas = "peas";
 export const chickpeas = "chickpeas";
-export const pistachio = 'pistachio';
+export const pistachios = 'pistachios';
 export const mungBeans = "mung beans";
-export const azukiBean = "azuki bean";
+export const azukiBeans = "azuki beans";
 export const oatmeal = "oatmeal";
 export const soybeans = "soybeans";
 export const blackbeans = "black beans";
+export const potatoes = "potatoes";
+export const purplepotatoes = "purple potatoes";
 
-export const redPeppers = "red peppers";
+export const redPeppers = "red peppers or paprika";
 export const yellowPeppers = "yellow peppers";
+export const greenPeppers = "green peppers";
 export const carrots = "carrots";
 export const celery = "celery";
 export const garlic = "garlic";
@@ -21,12 +24,14 @@ export const redOnion = "red onion";
 export const yellowOnion = "yellow onion";
 export const ginger = "ginger";
 export const cardamom = "cardamom";
-export const sesame = "sesame";
+export const sesame = "sesame seeds or sesame oil";
+export const mushrooms = "mushrooms";
+export const gobou = "gobou";
 
 export const coriander = "coriander";
 export const turmeric = "turmeric"; 
 export const cumin = "cumin";
-export const blackPeppercorn = "black peppercorn";
+export const blackPeppercorns = "black peppercorns";
 export const spanishOlives = "Spanish olives"; 
 export const kalamataOlives = "Kalamata olives"; 
 export const asian_solomonsSeal = "Solomon's seal"; // 玉竹
@@ -44,6 +49,7 @@ export const lemon = "lemon";
 export const apple = "apple";
 export const banana = "banana";
 export const blueberries = "blueberries";
+export const cranberries = "cranberries";
 
 export const coffee = "coffee";
 export const greenTea = "green tea";
@@ -51,6 +57,6 @@ export const blackTea = "black tea";
 export const assamTea = "assam tea";
 export const chocolate = "chocolate";
 
-export const natto = "natto";
+export const fermentedSoybeans = "fermented Soybeans";
 export const kimchi = "kimchi";
 
--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import * as items from './constants.js';
 import { logAll } from './utils/logAllUtils.js';
 import { ConstantSet } from './utils/setUtils.js';
 
-globalThis.d0701 = new ConstantSet([items.spinach, items.peas, items.redPeppers, items.carrots, items.garlic, items.lemon, items.mungBeans, items.coriander, items.blackPeppercorn, items.avocado, items.kiwi, items.cucumber, items.apple, items.coffee, items.pistachio, items.cumin]);
+globalThis.d0701 = new ConstantSet([items.spinach, items.peas, items.redPeppers, items.carrots, items.garlic, items.lemon, items.mungBeans, items.coriander, items.blackPeppercorns, items.avocado, items.kiwi, items.cucumber, items.apple, items.coffee, items.pistachios, items.cumin]);
 
 globalThis.d0702 = new ConstantSet([items.avocado, items.kiwi, items.chocolate, items.coffee, items.greenTea, items.cardamom, items.coriander, items.apple, items.carrots, items.spinach, items.peas, items.redPeppers, items.sesame]);
 
-globalThis.d0703 = new ConstantSet([items.asian_huangqi, items.gojiBerries, items.asian_daylily, items.asian_solomonsSeal, items.kale, items.haricotsVerts, items.tomatoes, items.oatmeal, items.apple, items.soybeans, items.broccoli, items.sesame, items.blackbeans, items.blueberries]);
+globalThis.d0703 = new ConstantSet([items.asian_huangqi, items.gojiBerries, items.asian_daylily, items.asian_solomonsSeal, items.kale, items.haricotsVerts, items.tomatoes, items.oatmeal, items.apple, items.soybeans, items.broccoli, items.sesame, items.blackbeans, items.blueberries, items.coffee]);
+
+globalThis.d0704 = new ConstantSet([items.potatoes, items.purplepotatoes, items.kiwi, items.apple, items.coffee, items.asian_huangqi, items.gojiBerries, items.asian_daylily, items.asian_solomonsSeal, items.kale, items.haricotsVerts, items.tomatoes, items.greenTea, items.assamTea, items.lemon, items.blueberries, items.cranberries, items.pistachios])
+
+globalThis.d0705 = new ConstantSet([items.broccoli, items.spinach, items.carrots, items.celery, items.yellowPeppers, items.greenPeppers, items.turmeric, items.redPeppers, items.cumin, items.mushrooms, items.gobou])
+
 
 logAll(7,1);
 
